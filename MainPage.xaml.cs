67c9aaa7393958362a69fdefb761458871c88865
Aaishalondt20 <5100170@mypillar7uwc.ac.za>
2024-02-21 13:02:10 +0200

    jdon

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO;
 using System.Text;
 using Newtonsoft.Json;
@@ -6,26 +7,77 @@ using Newtonsoft.Json.Linq;
 
 namespace JSONAssignment
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
-        int count = 0;
-
+        private string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "profile_creation.txt");
+        
         public MainPage()
         {
             InitializeComponent();
+            BindingContext= this;
         }
+        #region Profile Data
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        
+            public string _name
+            {
+                get => _name;
+
+                set
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public string _lastname
+            {
+                get => _lastname;
+
+                set
+                {
+                    _lastname = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public string _emailaddress
+            {
+                get => _emailaddress;
+
+                set
+                {
+                    _emailaddress = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public string _bio
+            {
+                get => _bio;
+
+                set
+                {
+                    _bio = value;
+                    OnPropertyChanged();
+                }
+            }
+            #endregion Profile Data
+        
+        #region Buttons
+
+      
+        private void SaveButton_Clicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            Profile profile = new Profile();
+         
         }
-    }
 
+        private void LoadButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion Buttons
+    }
 }
