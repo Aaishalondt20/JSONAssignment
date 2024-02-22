d2309509480fcf623244a40d76e5d0ea9d0f757e
Aaishalondt20 <5100170@mypillar7uwc.ac.za>
2024-02-22 11:56:26 +0200

    json

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,86 +7,110 @@ using Newtonsoft.Json.Linq;
 
 namespace JSONAssignment
 {
-    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    public partial class MainPage : ContentPage
     {
-        private string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "profile_creation.txt");
-        
+        /* private string _name;
+
+         public string Name
+         {
+             get { return _name; }
+             set { _name = value;
+
+                 OnPropertyChanged();
+             }
+         }
+
+         private string _lastname;
+
+         public string LastName
+         {
+             get { return _lastname; }
+             set { _lastname = value; 
+
+                 OnPropertyChanged();
+             }
+         }
+
+            private string _emailaddress;
+
+         public string EmailAddress
+         {
+             get { return _emailaddress; }
+             set { _emailaddress = value; 
+
+                 OnPropertyChanged();
+             }
+         }
+
+            private string _bio;
+
+         public string Bio
+         {
+             get { return _bio; }
+             set { _bio= value; 
+
+                 OnPropertyChanged();
+             }
+         }
+        */
+
+        private Profile _profile;
+        public Profile CurrentProfile
+        {
+            get { return _profile; }
+            set
+            {
+                _profile = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainPage()
         {
             InitializeComponent();
-            BindingContext= this;
+
+            //CurrentProfile = LoadData();
+
+            BindingContext = this;
+
         }
-        #region Profile Data
-        Profile profile = new Profile();   
-        public class Profile
-        { }
-            public string Name;
-            public string LastName;
-            public string EmailAddress;
-            public string Bio;
-            public string json;
-            public string Name
-            {
-                get => Name;
 
-                set
-                {
-                    Name = value;
-                    OnPropertyChanged();
-                }
-            }
 
-            public string Lastname
-            {
-                get => Lastname;
 
-                set
-                {
-                    LastName = value;
-                    OnPropertyChanged();
-                }
-            }
 
-            public string Emailaddress
-            {
-                get => Emailaddress;
-
-                set
-                {
-                    EmailAddress = value;
-                    OnPropertyChanged();
-                }
-            }
-
-            public string Bio
-            {
-                get => Bio;
-
-                set
-                {
-                    Bio = value;
-                    OnPropertyChanged();
-                }
-            }
-            #endregion Profile Data
-        
         #region Buttons
 
-      
-        private void SaveButton_Clicked(object sender, EventArgs e)
+        public void SaveProfile(Profile profile)
         {
-            string json = JsonConvert.SerializeObject(Profile);
-            File.WriteAllText(_fileName, json);
-            SaveButton.Text= File.ReadAllText(_fileName);
-         
+           string _fileName = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+           string FullFile = Path.Combine(_fileName, "Profile.txt");
+           string json = JsonConvert.SerializeObject(profile);
+           File.WriteAllText(_fileName, json);
+           SaveButton.Text = File.ReadAllText(_fileName);
+        }
+
+        public Profile LoadData() 
+        {
+            string _fileName = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string FullFile = Path.Combine(_fileName, "Profile.txt");
+
+            string FileContent = File.ReadAllText(FullFile);
+
+            Profile currentprofile = JsonConvert.DeserializeObject<Profile>(FileContent);
+            return currentprofile;
+        }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+           SaveProfile(this.CurrentProfile);
         }
 
         private void LoadButton_Clicked(object sender, EventArgs e)
         {
-            Profile deserializedprofile = JsonConvert.DeserializeObject<Profile>(json);
-            File.ReadAllText(_fileName);
+            LoadData();
         }
 
+        
         #endregion Buttons
     }
 }
---
+++ Profile.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONAssignment
+{
+    public class  Profile : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+
+        private string _lastname;
+        public string LastName
+        {
+            get { return _lastname; }
+
+            set
+            {
+                _lastname = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastName)));
+
+            }
+        }
+
+        private string _emailaddress;
+        public string EmailAddress
+        {
+            get { return _emailaddress; }
+
+            set
+            {
+                _emailaddress = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(EmailAddress)));
+            }
+        }
+
+        private string _bio;
+        public string Bio
+        {
+            get { return _bio; }
+
+            set
+            {
+                _bio = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Bio)));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+    }
+}
